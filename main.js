7eb676e5b98821727636f30b60c897c998f89cd0
Brendon <brendonpjon@gmail.com>
2023-09-01 15:56:39 +0100

    Singly & doubly linkedlist stacks and queues

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 class Node {
     constructor(value) {
         this.value = value
-        this.next = undefined
+        this.next = null
     }
 }
 
@@ -10,143 +10,81 @@ class Node {
 
 class LinkedList {
     constructor() {
-        this.head = undefined
-        this.tail = undefined
+        this.head = null
+        this.tail = null
         this.length = 0
     }
 
-    Prepend(value){
-        const nodeData = new Node(value)
-        this.length++
-        if(!this.head){
-            this.head = nodeData;
-            return;
-        }
-        nodeData.next = this.head;
-        this.head = nodeData;
-    }
-
-    Append(value){
+    Push(value){
         const nodeData = new Node(value)
         this.length++;
-        if(!this.tail)
+        if(!this.head)
         {
-            this.head = this.tail = nodeData;
+            this.head = nodeData;
+            this.tail = nodeData;
             return;
         }
-        this.tail.next = nodeData;
-        this.tail = nodeData;
+        else{
+            this.tail.next = nodeData;
+            this.tail = nodeData;
+            return;
+        }
     }
 
-    Remove(value){
+    Pop(){
         if(!this.head){
-            return console.log("value not in linked list")
+            return undefined
         }
-        if(this.head.value === value){
-            if(!this.head.next)
-            {
-                let head = this.head
-                this.head = this.tail = undefined;
-                this.length--
-                return head;
-            }
+        let temp = this.head
+        let pre = this.head
+        while( temp.next){
+            pre = temp
+            temp = temp.next
         }
-        let curr  = this.head;
-        for(let i = 0; i < this.length; ++i){
-            if(curr.next.value === value){
-                break;
-            }
-            if(curr.next === null)
-            {
-                return console.log("value not in linked list")
-            }
-            curr = curr.next
-        }
+        this.tail = pre
+        this.tail.next = null
         this.length--
-        if(curr.next.value === this.tail.value)
-        {
-            let tail = curr.next
-            curr.next = undefined
-            this.tail = curr;
-            return tail;
+        if(this.length === 0){
+            this.head = null
+            this.tail = null
         }
-        let item = curr
-        curr = curr.next
-        return item     
+        return temp
     }
 
-    Get(value){
-
-    }
-    
-    InsertAt(value, idx){
-        if(idx > this.length){
-            throw new Error("Index number is outside of the linked list")
+    Unshift(value){
+        const nodeData = new Node(value)
+        if(!this.head){
+            this.head = nodeData;
+            this.tail = nodeData;
         }
-        else if( idx === this.length){
-            this.Append(value);
-            return
-        }
-        else if( idx === 0){
-            this.Prepend(value);
-            return;
+        else{
+            nodeData.next = this.head;
+            this.head = nodeData;
         }
         this.length++
-        let curr = this.head
-        for(let i = 0; i < idx - 1; ++i){
-            curr = curr.next;
-        }
-        let nodeData = new Node(value);
-        nodeData.next = curr.next
-        curr.next = nodeData
+        return
     }
 
-    RemoveAt(location){ // revise not 100% correct needs more work...............
-        if( location > this.length || location < 0 ){
-            return undefined;
+    Shift(){
+        if(!this.head){
+            return undefined
         }
-        this.length--;
-        if(location === 0)
-        {
-         let head = this.head
-         this.head = this.head.next;
-         return head;  
+        let temp = this.head
+        this.head = this.head.next
+        temp.next = null
+        this.length--
+        if(this.length === 0){
+            this.tail = null
         }
-        if(location === this.length + 1)
-        {
-            let tail = this.head
-            while(tail.next.value !== this.tail.value)
-            {
-                tail = tail.next
-            }
-            let removeTail = this.tail
-            tail.next = null
-            this.tail = tail
-            return removeTail
-        }
-        let curr = this.head;
-        let index = 0
-        while((location - 1) >= index){
-            if(!curr.next)
-            {
-                return
-            }
-            curr = curr.next
-            index++
-        }
-        let fish  = curr.next
-        curr.next = curr.next.next
-        return fish
-        
-        
+        return temp
     }
-    }
+
+}
 
     let cat = new LinkedList()
 
-    cat.Append(4)
-    cat.Append(5)
-    cat.InsertAt(3,1)
-    cat.InsertAt(12,0)
-    console.log(cat.RemoveAt(2))
+    cat.Push(4)
+    cat.Push(5)
+    cat.Unshift(13)
+    cat.Shift()
     console.log(cat)
